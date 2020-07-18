7e2f59d8f7443c69f5a41d2579389f6eb371f741
Narraendharmass <67266747+Narraendharmass@users.noreply.github.com>
2020-07-18 17:35:07 +0530

    Add files via upload

---
+++ Firstmoduletest.java
@@ -0,0 +1,16 @@
+package testngseleniumprj;
+
+import org.testng.annotations.Test;
+
+public class Firstmoduletest {
+	@Test
+	public void MethodOneTest()
+	{
+		System.out.println("Test methodone of Firsttest");
+	}
+	@Test
+	public void MethodTwoTest()
+	{
+		System.out.println("Test methodtwo of Secondtest");
+	}
+}
